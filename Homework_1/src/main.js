01208baed4e17f13ae2086a1a402161f138894cc
Ichim Gabriel <ichimgabi97@gmail.com>
2024-03-20 14:15:49 +0200

    update functions

--- Homework_1/src/main.js
+++ Homework_1/src/main.js
@@ -1,35 +1,16 @@
-export function sumOfEvenNumbers(n) {
-  return -1;
-}
+export function sumOfEvenNumbers(n) {}
 
-export function sumOfOddNumbers(n) {
-    return -1;
-}
+export function sumOfOddNumbers(n) {}
 
-export function sumOfEvenPosInArray(arr) {
-    return -1;
-}
+export function sumOfEvenPosInArray(arr) {}
 
-export function sumOfOddPosInArray(arr) {
-    return -1;
-}
+export function sumOfOddPosInArray(arr) {}
 
-export function sumOfElements(arr1, arr2) {
-    return -1;
-}
+export function sumOfElements(arr1, arr2) {}
 
-export function isElementInArray(element, arr){
-    return -1;
-}
-export function isElementInArrayBinarySearch(el, arr){
-    return -1;
-}
+export function isElementInArray(element, arr) {}
+export function isElementInArrayBinarySearch(el, arr) {}
 
-export function isPalindrome(palindrome) {
-    return -1;
-}
+export function isPalindrome(palindrome) {}
 
-export function encrypt(toEncrypt, leftShift) {
-    return -1;
-}
-
+export function encrypt(toEncrypt, leftShift) {}
